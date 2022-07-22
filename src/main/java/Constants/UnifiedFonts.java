283ff578c87c9f31e6adc090eb9a20ff332fbc09
graykeng <graykeng7@gmail.com>
2022-07-21 23:32:31 -0700

    PopUpPanel in progress. Fonts done.

---
+++ src/main/java/Constants/UnifiedFonts.java
@@ -0,0 +1,33 @@
+package Constants;
+
+import java.awt.*;
+
+public class UnifiedFonts {
+    static String fontName = "Monaco";
+
+    public static Font font5P = new Font(fontName, Font.PLAIN, 5);
+    public static Font font10P = new Font(fontName, Font.PLAIN, 10);
+    public static Font font15P = new Font(fontName, Font.PLAIN, 15);
+    public static Font font20P = new Font(fontName, Font.PLAIN, 20);
+    public static Font font25P = new Font(fontName, Font.PLAIN, 25);
+    public static Font font30P = new Font(fontName, Font.PLAIN, 30);
+    public static Font font35P = new Font(fontName, Font.PLAIN, 35);
+    public static Font font40P = new Font(fontName, Font.PLAIN, 40);
+    public static Font font45P = new Font(fontName, Font.PLAIN, 45);
+    public static Font font50P = new Font(fontName, Font.PLAIN, 50);
+    public static Font font55P = new Font(fontName, Font.PLAIN, 55);
+    public static Font font60P = new Font(fontName, Font.PLAIN, 60);
+
+    public static Font font5B = new Font(fontName, Font.BOLD, 5);
+    public static Font font10B = new Font(fontName, Font.BOLD, 10);
+    public static Font font15B = new Font(fontName, Font.BOLD, 15);
+    public static Font font20B = new Font(fontName, Font.BOLD, 20);
+    public static Font font25B = new Font(fontName, Font.BOLD, 25);
+    public static Font font30B = new Font(fontName, Font.BOLD, 30);
+    public static Font font35B = new Font(fontName, Font.BOLD, 35);
+    public static Font font40B = new Font(fontName, Font.BOLD, 40);
+    public static Font font45B = new Font(fontName, Font.BOLD, 45);
+    public static Font font50B = new Font(fontName, Font.BOLD, 50);
+    public static Font font55B = new Font(fontName, Font.BOLD, 55);
+    public static Font font60B = new Font(fontName, Font.BOLD, 60);
+}
--- src/main/java/Panels/PopUpPanel.java
+++ src/main/java/Panels/PopUpPanel.java
@@ -1,54 +1,114 @@
 package Panels;
 
+import Constants.*;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class PopUpPanel extends JPanel {
-    private JPanel topSide;
+
+    private JScrollPane scrollPane;
+
     private JPanel scrollSection;
-    private JPanel space;
-    private JLabel image;
-    private JLabel userName;
-    private JLabel uID;
+    private JPanel profile;
     private JPanel information;
     private JPanel interests;
     private JPanel photoWall;
-    private JScrollPane scrollPane;
+
+    private JLabel image;
+    private JLabel userNameTXT;
+    private JLabel uIDTXT;
+    private JLabel infoTXT;
 
     public PopUpPanel(){
         prepareGUI();
     }
 
     private void prepareGUI(){
-        topSide = new JPanel();
         scrollSection = new JPanel();
-        space = new JPanel();
-        image = new JLabel();
-        userName = new JLabel();
-        uID = new JLabel();
+        profile = new JPanel();
         information = new JPanel();
         interests = new JPanel();
         photoWall = new JPanel();
 
-        userName.setText("USERNAME");
-        uID.setText("USERID");
+        image = new JLabel();
+        userNameTXT = new JLabel();
+        uIDTXT = new JLabel();
+        infoTXT = new JLabel();
 
-        topSide.setLayout(new GridLayout(2, 1));
-        topSide.add(userName);
-        topSide.add(uID);
 
-        scrollSection.setLayout(new GridLayout(3, 3));
-        scrollSection.add(space);
-        scrollSection.add(topSide);
+        /**
+         * Set the size and position for scrollPanel, information, interests, and photoWall
+         */
+        scrollSection.setLayout(null);
+        scrollSection.setLocation(0, 0);
+        scrollSection.setPreferredSize(new Dimension(Constants.WIDTH, Constants.HEIGHT*2));
+
+        profile.setLayout(null);
+        profile.setLocation(0, 0);
+        profile.setSize(Constants.WIDTH, Constants.HEIGHT/3);
+
+        information.setLayout(null);
+        information.setLocation(0, (int)profile.getPreferredSize().getHeight());
+        information.setSize(Constants.WIDTH, Constants.HEIGHT/3);
+
+        interests.setLayout(null);
+        interests.setLocation(0, (int)(profile.getPreferredSize().getHeight() + information.getPreferredSize().getHeight()));
+        interests.setSize(Constants.WIDTH, Constants.HEIGHT/3);
+
+        photoWall.setLayout(null);
+        photoWall.setLocation(0, (int)(profile.getPreferredSize().getHeight() + information.getPreferredSize().getHeight() + interests.getPreferredSize().getHeight()));
+        photoWall.setSize(Constants.WIDTH, Constants.HEIGHT);
+
+        /**
+         * Set the size, position, and content for Labels
+         */
+        userNameTXT.setFont(UnifiedFonts.font60B);
+        userNameTXT.setText("USERNAME");
+        userNameTXT.setLocation(getCentreX(userNameTXT.getPreferredSize().getWidth(), scrollSection.getPreferredSize().getWidth()), Constants.HEIGHT/12);
+        userNameTXT.setSize((int)userNameTXT.getPreferredSize().getWidth(), (int)userNameTXT.getPreferredSize().getHeight());
+        profile.add(userNameTXT);
+
+        uIDTXT.setFont(UnifiedFonts.font30P);
+        uIDTXT.setText("USERID");
+        uIDTXT.setLocation(getCentreX(userNameTXT.getPreferredSize().getWidth(), scrollSection.getPreferredSize().getWidth()), Constants.HEIGHT/12 + (int)userNameTXT.getPreferredSize().getHeight());
+        uIDTXT.setSize((int)uIDTXT.getPreferredSize().getWidth(), (int)uIDTXT.getPreferredSize().getHeight());
+        profile.add(uIDTXT);
+
+        infoTXT.setFont(UnifiedFonts.font30B);
+        infoTXT.setText("INFORMATION:");
+        infoTXT.setLocation(0, 0);
+        infoTXT.setSize((int)infoTXT.getPreferredSize().getWidth(), (int)infoTXT.getPreferredSize().getHeight());
+        information.add(infoTXT);
+
+        /**
+         * Add everything onto the scrollSection
+         */
+        scrollSection.add(profile);
+        scrollSection.add(information);
+        scrollSection.add(interests);
+        scrollSection.add(photoWall);
 
         scrollPane = new JScrollPane(
                 scrollSection,
                 ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
         );
-        scrollPane.setPreferredSize(new Dimension(600,  600));
+        scrollPane.setPreferredSize(new Dimension(Constants.WIDTH,  Constants.HEIGHT));
 
         this.add(scrollPane);
+    }
+
+    /**
+     * Get the X for the INSIDE element you want to set in the middle of the OUTSIDE element
+     * @param inside
+     * @param outside
+     * @return
+     */
+    private int getCentreX(Double inside, Double outside){
+        Double doubleValue = outside/2 - inside/2;
+        int intValue = doubleValue.intValue();
+        return intValue;
     }
 
 }
