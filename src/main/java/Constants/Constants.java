9455f1cc542a4797ecfe37ab653d87c03e7fa0ad
graykeng <graykeng7@gmail.com>
2022-07-22 15:00:28 -0700

    PopUpPanel interests done

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     // Screen
     public final static int WIDTH = 800;
     public final static int HEIGHT = 600;
+    public final static int SCROLL_CONTROLLER_WIDTH = 17;
 }
--- src/main/java/Panels/PopUpPanel.java
+++ src/main/java/Panels/PopUpPanel.java
@@ -13,6 +13,7 @@ public class PopUpPanel extends JPanel {
     private JPanel profile;
     private JPanel information;
     private JPanel interests;
+    private JPanel interestsGrid;
     private JPanel photoWall;
 
     private JLabel image;
@@ -22,7 +23,10 @@ public class PopUpPanel extends JPanel {
     private JLabel sexTXT;
     private JLabel birthdayTXT;
     private JLabel ageTXT;
+    private JLabel interestTXT;
+    private JButton singleInterest;
 
+    private String[] interestArray;
 
     public PopUpPanel(){
         prepareGUI();
@@ -33,6 +37,7 @@ public class PopUpPanel extends JPanel {
         profile = new JPanel();
         information = new JPanel();
         interests = new JPanel();
+        interestsGrid = new JPanel(new GridLayout(1, 4));
         photoWall = new JPanel();
 
         image = new JLabel();
@@ -42,7 +47,9 @@ public class PopUpPanel extends JPanel {
         sexTXT = new JLabel();
         birthdayTXT = new JLabel();
         ageTXT = new JLabel();
+        interestTXT = new JLabel();
 
+        interestArray = new String[]{"Basketball", "LOL", "WOW", "COOK"};
 
         /**
          * Set the size and position for scrollPanel, information, interests, and photoWall
@@ -90,8 +97,41 @@ public class PopUpPanel extends JPanel {
 
         sexTXT.setFont(UnifiedFonts.font20P);
         sexTXT.setText("SEX: M/F");
-        sexTXT.setLocation(0, (int)information.getPreferredSize().getHeight()/2);
-        //sexTXT.setSize((int)sexTXT.getPreferredSize().getWidth(), );
+        sexTXT.setLocation(0, (int)(information.getPreferredSize().getHeight()/2 - infoTXT.getPreferredSize().getHeight()));
+        sexTXT.setSize((int)sexTXT.getPreferredSize().getWidth(), (int)sexTXT.getPreferredSize().getWidth());
+        information.add(sexTXT);
+
+        birthdayTXT.setFont(UnifiedFonts.font20P);
+        birthdayTXT.setText("BIRTHDAY: //");
+        birthdayTXT.setLocation(getCentreX(birthdayTXT.getPreferredSize().getWidth(), information.getPreferredSize().getWidth()), (int)information.getPreferredSize().getHeight()/2);
+        birthdayTXT.setSize((int)birthdayTXT.getPreferredSize().getWidth(), (int)birthdayTXT.getPreferredSize().getHeight());
+        information.add(birthdayTXT);
+
+        ageTXT.setFont(UnifiedFonts.font20P);
+        ageTXT.setText("AGE: ");
+        ageTXT.setLocation((int)(information.getPreferredSize().getWidth()-ageTXT.getPreferredSize().getWidth())-Constants.SCROLL_CONTROLLER_WIDTH, (int)information.getPreferredSize().getHeight()/2);
+        ageTXT.setSize((int)ageTXT.getPreferredSize().getWidth(), (int)ageTXT.getPreferredSize().getHeight());
+        information.add(ageTXT);
+
+        interestTXT.setFont(UnifiedFonts.font30B);
+        interestTXT.setText("INTERESTS:");
+        interestTXT.setLocation(0, 0);
+        interestTXT.setSize((int)interestTXT.getPreferredSize().getWidth(), (int)interestTXT.getPreferredSize().getHeight());
+        interests.add(interestTXT);
+
+        /**
+         * Add all the interests onto the interest panel
+         */
+        for(int i = 0; i < interestArray.length; i++){
+            singleInterest = new JButton();
+            singleInterest.setFont(UnifiedFonts.font20P);
+            singleInterest.setText(interestArray[i]);
+            singleInterest.setText(interestArray[i]);
+            interestsGrid.add(singleInterest);
+        }
+        interestsGrid.setLocation(0, (int)interestTXT.getPreferredSize().getHeight());
+        interestsGrid.setSize(Constants.WIDTH - Constants.SCROLL_CONTROLLER_WIDTH, (int)singleInterest.getPreferredSize().getHeight());
+        interests.add(interestsGrid);
 
         /**
          * Add everything onto the scrollSection
