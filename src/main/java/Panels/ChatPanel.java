89906d64a602c28b4aef0108c19402415ee960be
sibeiz <sibeiz@sfu.ca>
2022-07-21 21:55:38 -0700

    ChatPanel and ChatSelectPanel upload

--- src/main/java/Panels/ChatPanel.java
+++ src/main/java/Panels/ChatPanel.java
@@ -1,4 +1,55 @@
 package Panels;
 
-public class ChatPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ChatPanel extends JPanel {
+    private JPanel Title;
+    private JPanel msgSendPanel;
+    private JScrollPane scrollPanel;
+
+    public ChatPanel() {
+        // Setting
+        this.setSize(250,600);
+        BorderLayout layout = new BorderLayout();
+        this.setLayout(layout);
+
+        // Title panel
+        Title = new JPanel();
+        Title.setSize(250,100);
+        JLabel tileLabel = new JLabel("FriendName1");
+        Title.add(tileLabel);
+
+        // scrollPanel
+        String strMsg1 = "A: Hi, nice to meet you!\n";
+        String strMsg2 = "B: Nice to meet you too!\n";
+        String strMsg3 = "A: The cover page was complete and was included in the original assignment submission without the TA having to request it. The cover page was complete and was included in the original assignment submission without the TA having to request it.The cover page was complete and was included in the original assignment submission without the TA having to request it";
+
+        JTextArea jta = new JTextArea();
+        jta.setLineWrap(true);
+        jta.setEditable(false);
+        jta.setBackground(new Color(238,238,238));
+        jta.setText(strMsg1+strMsg2+strMsg3);
+
+        jta.setFont(new Font(null, Font.PLAIN, 15));
+        scrollPanel = new JScrollPane(
+                jta,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        scrollPanel.setSize(250,400);
+
+        // msgPanel
+        msgSendPanel = new JPanel();
+        msgSendPanel.setSize(250,100);
+        msgSendPanel.setLayout(new BorderLayout());
+        JTextArea textArea = new JTextArea();
+        JButton addFileButton = new JButton("Send File");
+        msgSendPanel.add(textArea,BorderLayout.CENTER);
+        msgSendPanel.add(addFileButton, BorderLayout.EAST);
+
+        this.add(Title, BorderLayout.NORTH);
+        this.add(scrollPanel, BorderLayout.CENTER);
+        this.add(msgSendPanel, BorderLayout.SOUTH);
+    }
 }
--- src/main/java/Panels/ChatSelectPanel.java
+++ src/main/java/Panels/ChatSelectPanel.java
@@ -1,4 +1,57 @@
 package Panels;
 
-public class ChatSelectPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ChatSelectPanel extends JPanel {
+    private JPanel Title;
+    private JScrollPane scrollPanel;
+
+    public ChatSelectPanel(){
+        // Setting
+        this.setSize(250,600);
+        BorderLayout layout = new BorderLayout();
+        this.setLayout(layout);
+
+        // Title panel
+        Title = new JPanel();
+        Title.setSize(250,100);
+        JLabel tileLabel = new JLabel("UserName"+"(uid:123456789)");
+        Title.add(tileLabel);
+
+        // scrollPanel
+        int rows = 10;
+        String friendsName[] = {"Gray", "Lester", "Kerla","Austin","Gray", "Lester", "Kerla","Austin","Gray", "Lester"};
+        JPanel FriendsPanel = new JPanel(null);
+        FriendsPanel.setPreferredSize(new Dimension(380,75*rows));
+        if (rows == 0) {
+            String empty = "You have no friend, add your friend now!";
+            JLabel showEmpty = new JLabel(empty);
+            FriendsPanel.add(showEmpty, BorderLayout.CENTER);
+        }
+        for (int i = 0; i < rows; i++) {
+            JPanel row = new JPanel(new FlowLayout());
+            row.setSize(380,75);
+            row.setLocation(0,75*i);
+            String str = friendsName[i];
+            JLabel FriendLabel = new JLabel(str);
+
+
+            row.add(FriendLabel, BorderLayout.NORTH);
+
+            row.setBorder(BorderFactory.createTitledBorder(""));
+            FriendsPanel.add(row);
+        }
+
+        scrollPanel = new JScrollPane(
+                FriendsPanel,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        scrollPanel.setSize(250,400);
+
+
+        this.add(Title, BorderLayout.NORTH);
+        this.add(scrollPanel, BorderLayout.CENTER);
+    }
 }
